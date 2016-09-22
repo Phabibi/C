50a0e9018744eecd6aae1841d7b608fb7d4fa711
phabibi <phabibi@asb9838nu-c04.csil.sfu.ca>
2016-09-21 17:24:39 -0700

    t6.c

---
+++ 1/t6.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+
+void main()	{
+	float a;
+	float sum = 0.0;
+	int count=0 ;
+	float max = -99999;
+	float min = 99999;
+
+
+	while ( (scanf("%f\n", &a) != EOF ) ){
+
+		sum+=a;
+		count+=1;
+
+		if (max < a) {
+			max = a ;
+		}
+		if ( min > a ) {
+				min = a ;
+			}
+
+
+	}
+
+	printf("%.2f %.2f %.2f \n", min , max , sum/count);
+}
