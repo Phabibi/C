fd615e298d152140c1245bca7207b0ed6b5597c6
phabibi <phabibi@asb9838nu-c14.csil.sfu.ca>
2016-11-05 17:11:40 -0700

    suuuuuuh dude

--- 4/t2.c
+++ 4/t2.c
@@ -14,18 +14,19 @@ void draw_rectangle( uint8_t array[],
 
 
   // negative cases
-  if (rect_width < 0)           //right negative
+  if (rect_width < 0)
   {
-    x = x + rect_width;
+    x = x + rect_width + 1 ;
   }
 
-  if (rect_height < 0)          //left negative
+  if (rect_height < 0)
   {
-    y = y + rect_height;
+    y = y + rect_height + 1;
   }
 
-  if (rect_width < 0 || rect_height < 0)    // either or negative
+  if (rect_width < 0 || rect_height < 0)
   {
+    //printf("x = %d , y=%d",x,y);
     draw_rectangle(array, cols, rows, x,y, abs(rect_width),abs(rect_height), color );
   }
 
@@ -37,28 +38,32 @@ void draw_rectangle( uint8_t array[],
   {
       for(i = y; i <= (y + rect_height) ; i++)
       {
+        //printf("Drawing row %d \n", i);
 
         if (i >= 0  && j >= 0)
         {
         array[x + i*cols] = color;
         }
 
-        for(j = x ; j < (x + rect_width) ; j++)
+        for(j = x ; j < x + rect_width - 1  ; j++)             //draws rows (each row must have 0 or len-1)
         {
+          //printf("Drawing column %d \n", j);
           if(i == y || i == (y + rect_height) )    // empty region (middle)
           {
             if( i >= 0 && j >= 0)
-              {
-                array[j + i *cols] = color;
-              }
+            {
+              array[j + i *cols] = color;
+            }
           }
-
         }
-          if(i >= 0 && j >= 0)
-          {
-          array[j + i*cols] = color;
-          }
-      }
+        if( i >= 0 && j >= 0)
+        {
+          array[j + i *cols] = color;
+        }
+
+        j = x;
+    }
 
   }
+
 }
